6b2dc03d846d40fc8e32c155b920bafecdc45bda
rcarlsson <rcarlsson@5c723cce-c907-0410-9aa2-ab8348fff7a4>
2008-11-29 17:01:30 +0000

    gen_serverified and added functions for getting and setting the poll time dynamically
    git-svn-id: http://svn.process-one.net/contribs/trunk/eunit@276 5c723cce-c907-0410-9aa2-ab8348fff7a4

--- src/file_monitor.erl
+++ src/file_monitor.erl
@@ -20,87 +20,37 @@
 %% @copyright 2006 Richard Carlsson
 %% @doc Erlang file monitoring service
 
-%% The behaviour of this service is inspired by the open source FAM
-%% daemon [http://oss.sgi.com/projects/fam/].
-
 -module(file_monitor).
 
--export([start/0, start/1, start/2, stop/0, stop/1, monitor_file/2,
-	 monitor_file/3, monitor_dir/2, monitor_dir/3, demonitor/1,
-	 demonitor/2]).
+-behaviour(gen_server).
 
--export([main/1]).    %% private
+-export([start/0, start/1, start/2, start_link/0, start_link/1,
+	 start_link/2, stop/0, stop/1, init/1, monitor_file/2,
+	 monitor_file/3, monitor_dir/2, monitor_dir/3, demonitor/1,
+	 demonitor/2, get_poll_time/0, get_poll_time/1, set_poll_time/1,
+	 set_poll_time/2]).
+
+-export([handle_call/3, handle_cast/2, handle_info/2, code_change/3,
+	 terminate/2]).
+
+%% -export([test/0]).
 
 -include_lib("kernel/include/file.hrl").
 
--define(POLL_TIME, 5000). % default; change with option poll_time
--define(SERVER, file_monitor).
 
-%% NOTE: paths should be absolute, but this is not checked
-
-%% We don't change the paths, e.g. from relative to absolute, but we
-%% make sure that the path is a flat string and return it to the caller.
-
-monitor_file(Path, Pid) ->
-    monitor_file(?SERVER, Path, Pid).
-
-monitor_file(Server, Path, Pid) ->
-    monitor(Server, file, filename:flatten(Path), Pid).
-
-monitor_dir(Path, Pid) ->
-    monitor_dir(?SERVER, Path, Pid).
-
-monitor_dir(Server, Path, Pid) ->
-    monitor(Server, dir, filename:flatten(Path), Pid).
-
-monitor(Server, Type, Path, Pid) when is_pid(Pid) ->
-    {ok, Ref} = command(Server, {monitor, {Type, Path}, Pid}),
-    {ok, Path, Ref}.
-
-demonitor(Ref) ->
-    demonitor(?SERVER, Ref).
-
-demonitor(Server, Ref) ->
-    ok = command(Server, {demonitor, Ref}).
+%% The behaviour of this service is inspired by the open source FAM
+%% daemon [http://oss.sgi.com/projects/fam/].
+%%
+%% NOTE: Monitored paths should be absolute, but this is not checked.
+%% 
+%% We never change the paths, e.g. from relative to absolute, but we
+%% make sure that the path is a flat string, for the sake of
+%% comparisons, and return this to the caller for reference.
 
 
-command(Server, Cmd) ->
-    ServerPid = ensure_started(Server),
-    ServerPid ! {command, self(), Cmd},
-    receive
-	{ServerPid, Result} -> Result
-    end.
-
-
-stop() ->
-    stop(?SERVER).
-
-stop(Server) ->
-    Server ! stop,
-    ok.
-	    
-ensure_started(Name) when is_atom(Name) ->
-    start(Name, []);
-ensure_started(Pid) when is_pid(Pid) ->
-    Pid.
-
-start() ->
-    start([]).
-
-start(Options) ->
-    start(?SERVER, Options).
-
-start(Name, Options) ->
-    case whereis(Name) of
-	undefined ->
-	    Parent = self(),
-	    Pid = spawn(fun () -> server_init(Name, Parent, Options) end),
-	    receive
-		{Pid, ok} -> Pid;
-		{Pid, error} -> throw(no_server)
-	    end;
-	Pid -> Pid
-    end.
+-define(DEFAULT_POLL_TIME, 5000).  % change with option poll_time
+-define(MIN_POLL_TIME, 100).
+-define(SERVER, ?MODULE).
 
 -record(state, {poll_time,  % polling interval (milliseconds)
 		dirs,       % map: directory path -> entry
@@ -109,60 +59,147 @@ start(Name, Options) ->
 		clients     % map: client Pid -> erlang:monitor/2 Ref
 	       }).
 
-server_init(undefined, Parent, Options) ->
-    %% anonymous server
-    server_init_1(Parent, Options);
-server_init(Name, Parent, Options) ->
-    case catch register(Name, self()) of
-	true ->
-	    server_init_1(Parent, Options);
-	_ ->
-	    Parent ! {self(), error},
-	    exit(failed)
-    end.
+-record(monitor, {pid, reference}).
 
-server_init_1(Parent, Options) ->
-    Parent ! {self(), ok},
-    server(set_timer(init_state(Options))).
+-record(entry, {info = undefined, files = [], monitors = sets:new()}).
 
-init_state(Options) ->
-    Time = case proplists:get_value(poll_time, Options) of
-	       N when is_integer(N), N >= 100 -> N;
-	       _ -> ?POLL_TIME
-	   end,
-    #state{poll_time = Time,
-	   dirs = dict:new(),
-	   files = dict:new(),
-	   clients = dict:new(),
-	   refs = dict:new()}.
 
-server(St) -> ?MODULE:main(St).
+%%
+%% User interface
+%%
 
-%% @private
-main(St) ->
-    receive
-	{command, From, {monitor, Object, Pid}} when is_pid(Pid) ->
-	    {Ref, St1} = new_monitor(Object, Pid, St),
-	    server_reply(From, {ok, Ref}),
-	    server(add_client(Pid, St1));
-	{command, From, {demonitor, Ref}} ->
-	    server_reply(From, ok),
-	    server(delete_monitor(Ref, St));
-	stop ->
-	    exit(normal);
-	time ->
-	    server(set_timer(poll(St)));
-	{'DOWN', _Ref, process, Pid, _Info} ->
-	    server(del_client(Pid, St));
-	_ ->
-	    server(St)
-    end.
+monitor_file(Path, Pid) ->
+    monitor_file(?SERVER, Path, Pid).
 
-server_reply(To, Msg) ->
-    To ! {self(), Msg}.
+monitor_file(Server, Path, Pid) ->
+    monitor(Server, file, Path, Pid).
+
+
+monitor_dir(Path, Pid) ->
+    monitor_dir(?SERVER, Path, Pid).
+
+monitor_dir(Server, Path, Pid) ->
+    monitor(Server, dir, Path, Pid).
+
+
+monitor(Server, Type, Path, Pid) when is_pid(Pid) ->
+    Cmd = {monitor, {Type, filename:flatten(Path)}, Pid},
+    {ok, Ref} = gen_server:call(Server, Cmd),
+    {ok, Path, Ref}.
+
+
+demonitor(Ref) ->
+    demonitor(?SERVER, Ref).
+
+demonitor(Server, Ref) ->
+    ok = gen_server:call(Server, {demonitor, Ref}).
+
+
+get_poll_time() ->
+    get_poll_time(?SERVER).
+
+get_poll_time(Server) ->
+    gen_server:call(Server, get_poll_time).
+
+
+set_poll_time(Time) ->
+    set_poll_time(?SERVER, Time).
+
+set_poll_time(Server, Time) ->
+    gen_server:call(Server, {set_poll_time, Time}).
+    
+
+
+start() ->
+    start([]).
+
+start(Options) ->
+    start({local, ?SERVER}, Options).
+
+start(undefined, Options) ->
+    gen_server:start(?MODULE, Options, []);
+start(Name, Options) ->
+    gen_server:start(Name, ?MODULE, Options, []).
+
+start_link() ->
+    start_link([]).
+
+start_link(Options) ->
+    start_link({local, ?SERVER}, Options).
+
+start_link(undefined, Options) ->
+    gen_server:start_link(?MODULE, Options, []);
+start_link(Name, Options) ->
+    gen_server:start_link(Name, ?MODULE, Options, []).
+
+
+stop() ->
+    stop(?SERVER).
+
+stop(Server) ->
+    gen_server:call(Server, stop),
+    ok.
+
+
+%%
+%% gen_server callbacks
+%%
+
+init(Options) ->
+    Time = safe_poll_time(proplists:get_value(poll_time, Options)),
+    St = #state{poll_time = Time,
+		dirs = dict:new(),
+		files = dict:new(),
+		clients = dict:new(),
+		refs = dict:new()},
+    set_timer(St),
+    {ok, St}.
+
+
+handle_call({monitor, Object, Pid}, _From, St) when is_pid(Pid) ->
+    {Ref, St1} = new_monitor(Object, Pid, St),
+    {reply, {ok, Ref}, add_client(Pid, St1)};
+handle_call({demonitor, Ref}, _From, St) when is_reference(Ref) ->
+    {reply, ok, delete_monitor(Ref, St)};
+handle_call(get_poll_time, _From, St) ->
+    {reply, St#state.poll_time, St};
+handle_call({set_poll_time, Time}, _From, St) ->
+    {reply, ok, St#state{poll_time=safe_poll_time(Time)}};
+handle_call(stop, _From, St) ->
+    {stop, normal, ok, St}.
+
+handle_cast(_, St) ->
+    {noreply, St}.
+
+handle_info(poll_time, St) ->  
+    {noreply, set_timer(poll(St))};
+handle_info({'DOWN', _Ref, process, Pid, _Info}, St) ->
+    {noreply, del_client(Pid, St)};
+handle_info(_, St) ->
+    {noreply, St}.
+
+code_change(_OldVsn, St, _Extra) ->
+    {ok, St}.
+
+terminate(_Reason, _St) ->
+    ok.
+
+%%
+%% Internal functions
+%%
+
+max(X, Y) when X > Y -> X;
+max(_, Y) -> Y.
+
+min(X, Y) when X < Y -> X;
+min(_, Y) -> Y.
+
+safe_poll_time(N) when is_integer(N) ->
+    min(16#FFFFffff, max(N, ?MIN_POLL_TIME));
+safe_poll_time(_) -> ?DEFAULT_POLL_TIME.
 
 set_timer(St) ->
-    erlang:send_after(St#state.poll_time, self(), time),
+    erlang:send_after(St#state.poll_time, self(), poll_time),
     St.
 
 
@@ -177,8 +214,6 @@ add_client(Pid, St) ->
 	    St#state{clients = dict:store(Pid, Ref, St#state.clients)}
     end.
 
-%% deleting a client
-
 del_client(Pid, St0) ->
     St = purge_pid(Pid, St0),
     case dict:find(Pid, St#state.clients) of
@@ -189,10 +224,6 @@ del_client(Pid, St0) ->
 	    St
     end.
 
-
--record(monitor, {pid, reference}).
-
--record(entry, {info = undefined, files = [], monitors = sets:new()}).
 
 new_monitor(Object, Pid, St) ->
     Ref = make_ref(),
@@ -414,3 +445,11 @@ cast(Msg, Monitors) ->
 		      Msg  % note that this is a fold, not a map
 	      end,
 	      Msg, Monitors).
+
+%% test() ->
+%%     Pid = spawn(fun loop/0),
+%%     register(fred,Pid).
+
+%% loop() ->
+%%     receive X -> erlang:display(X) end,
+%%     loop().
